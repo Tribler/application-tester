5066deadeaaeb0f1cb07f52f62db02e4202c4674
Martijn de Vos <mdmartijndevos@gmail.com>
2018-05-23 16:57:36 +0200

    Defined various new actions

---
+++ actions/browse_discovered_action.py
@@ -0,0 +1,18 @@
+from action_sequence import ActionSequence
+from actions.page_action import PageAction
+from actions.scroll_action import RandomScrollAction
+from actions.wait_action import WaitAction
+
+
+class BrowseDiscoveredAction(ActionSequence):
+    """
+    This action scrolls through the discovered torrents in Tribler.
+    """
+
+    def __init__(self):
+        super(BrowseDiscoveredAction, self).__init__()
+
+        self.add_action(PageAction('discovered'))
+        for _ in xrange(0, 10):
+            self.add_action(RandomScrollAction("window.discovered_channels_list"))
+            self.add_action(WaitAction(300))
