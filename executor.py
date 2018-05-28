6099b0dcd4737ba1c853a24c1988a2f085a1896f
Martijn de Vos <mdmartijndevos@gmail.com>
2018-05-28 16:47:50 +0200

    Added action to change anonymity of rand. download

--- executor.py
+++ executor.py
@@ -6,6 +6,7 @@ from random import random, randint, choice
 from twisted.internet.task import LoopingCall
 
 from actions.browse_discovered_action import BrowseDiscoveredAction
+from actions.change_anonymity_action import ChangeAnonymityAction
 from actions.explore_channel_action import ExploreChannelAction
 from actions.explore_download_action import ExploreDownloadAction
 from actions.page_action import RandomPageAction
@@ -91,7 +92,9 @@ class Executor(object):
         This method performs a random action in Tribler. There are various actions possible that can occur with
         different probabilities.
         """
-        probs = [('random_page', 30), ('search', 15), ('start_download', 20), ('remove_download', 5), ('explore_download', 10), ('browse_discovered', 5), ('explore_channel', 10), ('start_vod', 5)]
+        probs = [('random_page', 25), ('search', 15), ('start_download', 20), ('remove_download', 5),
+                 ('explore_download', 10), ('browse_discovered', 5), ('explore_channel', 10), ('start_vod', 5),
+                 ('change_anonymity', 5)]
         action = self.weighted_choice(probs)
         self._logger.info("Performing action: %s", action)
         if action == 'random_page':
@@ -112,5 +115,7 @@ class Executor(object):
             action = ScreenshotAction()
         elif action == 'start_vod':
             action = StartVODAction()
+        elif action == 'change_anonymity':
+            action = ChangeAnonymityAction()
 
         self.execute_action(action)
