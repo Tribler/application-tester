b1c82417b107df1c4c22704e456489b5ff8ef6e9
Martijn de Vos <mdmartijndevos@gmail.com>
2018-06-13 17:01:16 +0200

    Improved application test
    Added IRC client and two-way communication between the Tribler instance and application tester. Also added a check whether Tribler has crashed.

--- executor.py
+++ executor.py
@@ -3,10 +3,12 @@ import os
 from bisect import bisect
 from random import random, randint, choice
 
+from twisted.internet.defer import Deferred
 from twisted.internet.task import LoopingCall
 
 from actions.browse_discovered_action import BrowseDiscoveredAction
 from actions.change_anonymity_action import ChangeAnonymityAction
+from actions.check_crash_action import CheckCrashAction
 from actions.explore_channel_action import ExploreChannelAction
 from actions.explore_download_action import ExploreDownloadAction
 from actions.page_action import RandomPageAction
@@ -16,31 +18,70 @@ from actions.start_download_action import StartRandomDownloadAction
 from actions.remove_download_action import RemoveRandomDownloadAction
 from actions.start_vod_action import StartVODAction
 from actions.subscribe_unsubscribe_action import SubscribeUnsubscribeAction
+from ircclient import IRCManager
 
 
 class Executor(object):
 
-    def __init__(self, tribler_path, allow_plain_downloads):
+    def __init__(self, tribler_path, allow_plain_downloads, irc_id):
         self.tribler_path = tribler_path
         self._logger = logging.getLogger(self.__class__.__name__)
         self.allow_plain_downloads = allow_plain_downloads
+        self.pending_tasks = {}  # Dictionary of pending tasks
 
         self.random_action_lc = LoopingCall(self.perform_random_action)
         self.random_action_lc.start(15)
 
         self.check_task_completion_lc = LoopingCall(self.check_task_completion)
-        self.check_task_completion_lc.start(2)
+        self.check_task_completion_lc.start(2, now=False)
+
+        self.check_crash_lc = LoopingCall(self.check_crash)
+        self.check_crash_lc.start(10, now=False)
+
+        self.irc_manager = None
+
+        if irc_id:
+            self.irc_manager = IRCManager(irc_id)
+            self.irc_manager.start()
+
+    def check_crash(self):
+        """
+        Check whether the Tribler instance has crashed.
+        """
+        def on_crash_result(result):
+            if result:
+                self._logger.error("Tribler crashed! Stack trace: %s", result)
+                # Stop the execution of random actions and send a message to the IRC
+                self.random_action_lc.stop()
+                if self.irc_manager:
+                    self.irc_manager.irc.send_channel_message("Tribler crashed with stack trace: %s" % result)
+
+        self.execute_action(CheckCrashAction()).addCallback(on_crash_result)
 
     def check_task_completion(self):
         """
         This method periodically checks whether Python scripts have been completed.
         Completion of such a script is indicated by presence of a .done file.
         """
-        for file in os.listdir(os.path.join(os.getcwd(), "tmp_scripts")):
-            if file.endswith(".done"):
-                task_id = file[:-5]
+        for done_file_name in os.listdir(os.path.join(os.getcwd(), "tmp_scripts")):
+            if done_file_name.endswith(".done"):
+                task_id = done_file_name[:-5]
                 self._logger.info("Task with ID %s completed!", task_id)
-                os.remove(os.path.join(os.getcwd(), "tmp_scripts", file))
+
+                # Read the contents of the file
+                file_content = None
+                with open(os.path.join(os.getcwd(), "tmp_scripts", done_file_name)) as done_file:
+                    file_content = done_file.read()
+
+                if not file_content:
+                    file_content = None
+
+                # Invoke the callback
+                if task_id in self.pending_tasks:
+                    self.pending_tasks[task_id].callback(file_content)
+                    self.pending_tasks.pop(task_id, None)
+
+                os.remove(os.path.join(os.getcwd(), "tmp_scripts", done_file_name))
 
                 python_file_path = os.path.join(os.getcwd(), "tmp_scripts", "%s.py" % task_id)
                 if os.path.exists(python_file_path):
@@ -62,11 +103,14 @@ class Executor(object):
 
     def execute_action(self, action):
         """
-        Execute a given action.
+        Execute a given action and return a deferred that fires with the result of the action.
         """
         self._logger.info("Executing action: %s" % action)
 
         task_id = ''.join(choice('0123456789abcdef') for _ in xrange(10))
+        task_deferred = Deferred()
+        self.pending_tasks[task_id] = task_deferred
+
         tmp_scripts_dir = os.path.join(os.getcwd(), "tmp_scripts")
         if not os.path.exists(tmp_scripts_dir):
             os.makedirs(tmp_scripts_dir)
@@ -77,9 +121,13 @@ class Executor(object):
         if os.name == 'nt':
             destination = destination.replace('\\', '\\\\')
 
-        code = """def exit_script():
+        code = """return_value = ''
+
+def exit_script():
     import sys
-    open('%s', 'a').close()
+    global return_value
+    with open('%s', 'a') as done_file:
+        done_file.write(return_value)
     sys.exit(0)\n\n""" % destination
 
         code += action.generate_code() + '\nexit_script()'
@@ -90,6 +138,8 @@ class Executor(object):
 
         # Let Tribler execute this code
         self.execute_code(code_file_path)
+
+        return task_deferred
 
     def execute_code(self, code_file_path):
         self._logger.info("Executing code file: %s" % code_file_path)
