3aef7835c924cd4056f2a5beac19fac2c6031825
Martijn de Vos <mdmartijndevos@gmail.com>
2018-08-14 15:56:36 +0200

    Improved graph output, added x/y labels

--- scripts/downloads.r
+++ scripts/downloads.r
@@ -8,7 +8,7 @@ if(file.exists("output/download_stats.csv")){
     # Speed up
     p <- ggplot(df) + theme_bw()
     p <- p + geom_line(aes(x=time, y=speed_up, group=infohash, colour=infohash))
-    p <- p + theme(legend.position="bottom", legend.direction="horizontal")
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Upload speed (kb/s)")
     p
 
     ggsave(file="output/speed_up.png", width=8, height=6, dpi=100)
@@ -16,7 +16,7 @@ if(file.exists("output/download_stats.csv")){
     # Speed down
     p <- ggplot(df) + theme_bw()
     p <- p + geom_line(aes(x=time, y=speed_down, group=infohash, colour=infohash))
-    p <- p + theme(legend.position="bottom", legend.direction="horizontal")
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Download speed (kb/s)")
     p
 
     ggsave(file="output/speed_down.png", width=8, height=6, dpi=100)
--- scripts/resources.r
+++ scripts/resources.r
@@ -5,7 +5,7 @@ if(file.exists("output/cpu_stats.csv")){
 
     p <- ggplot(df) + theme_bw()
     p <- p + geom_line(aes(x=time, y=cpu_usage))
-    p <- p + theme(legend.position="bottom", legend.direction="horizontal")
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("CPU usage (%)")
     p
 
     ggsave(file="output/cpu_history.png", width=8, height=6, dpi=100)
@@ -13,10 +13,11 @@ if(file.exists("output/cpu_stats.csv")){
 
 if(file.exists("output/memory_stats.csv")){
 	df <- read.csv("output/memory_stats.csv", sep=",", header=T)
+    df$memory_usage = df$memory_usage / 1024 / 1024
 
     p <- ggplot(df) + theme_bw()
     p <- p + geom_line(aes(x=time, y=memory_usage))
-    p <- p + theme(legend.position="bottom", legend.direction="horizontal")
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Memory usage (MB)")
     p
 
     ggsave(file="output/memory_history.png", width=8, height=6, dpi=100)
