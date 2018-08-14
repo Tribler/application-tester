c95c347c5d61a33187e3c576913db80df04a29bc
Martijn de Vos <mdmartijndevos@gmail.com>
2018-08-14 14:52:26 +0200

    Added post-process download stats script

---
+++ scripts/downloads.r
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+if(file.exists("output/download_stats.csv")){
+	df <- read.csv("output/download_stats.csv", sep=",", header=T)
+	df$speed_up = df$speed_up / 1024
+	df$speed_down = df$speed_down / 1024
+
+    # Speed up
+    p <- ggplot(df) + theme_bw()
+    p <- p + geom_line(aes(x=time, y=speed_up, group=infohash, colour=infohash))
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal")
+    p
+
+    ggsave(file="output/speed_up.png", width=8, height=6, dpi=100)
+
+    # Speed down
+    p <- ggplot(df) + theme_bw()
+    p <- p + geom_line(aes(x=time, y=speed_down, group=infohash, colour=infohash))
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal")
+    p
+
+    ggsave(file="output/speed_down.png", width=8, height=6, dpi=100)
+}
