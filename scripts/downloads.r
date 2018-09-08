87b31472f251890cd2141e84aca41a78355301b9
Martijn de Vos <mdmartijndevos@gmail.com>
2018-09-08 13:37:59 +0200

    Added titles to graphs

--- scripts/downloads.r
+++ scripts/downloads.r
@@ -9,7 +9,7 @@ if(file.exists("output/download_stats.csv")){
     # Speed up
     p <- ggplot(df) + theme_bw()
     p <- p + geom_line(aes(x=time, y=speed_up, group=infohash, colour=infohash))
-    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Upload speed (kb/s)")
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Upload speed (kb/s)") + ggtitle("Upload speed of downloads")
     p
 
     ggsave(file="output/speed_up.png", width=8, height=6, dpi=100)
@@ -17,15 +17,15 @@ if(file.exists("output/download_stats.csv")){
     # Speed down
     p <- ggplot(df) + theme_bw()
     p <- p + geom_line(aes(x=time, y=speed_down, group=infohash, colour=infohash))
-    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Download speed (kb/s)")
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Download speed (kb/s)") + ggtitle("Download speed of downloads")
     p
 
     ggsave(file="output/speed_down.png", width=8, height=6, dpi=100)
 
     # Progress
     p <- ggplot(df) + theme_bw()
-    p <- p + geom_line(aes(x=time, y=dl_progress, group=dl_progress, colour=dl_progress))
-    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Progress (%)")
+    p <- p + geom_line(aes(x=time, y=dl_progress, group=infohash, colour=infohash))
+    p <- p + theme(legend.position="bottom", legend.direction="horizontal") + xlab("Time into experiment (sec)") + ylab("Progress (%)") + ylim(c(0, 100)) + ggtitle("Progress of downloads")
     p
 
     ggsave(file="output/progress.png", width=8, height=6, dpi=100)
