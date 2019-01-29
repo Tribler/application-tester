ef47d917db2f00bf1762460720207f4f02f74594
Martijn de Vos <mdmartijndevos@gmail.com>
2019-01-29 16:00:22 +0100

    Made the application tester gigachannel compatible

--- actions/subscribe_unsubscribe_action.py
+++ actions/subscribe_unsubscribe_action.py
@@ -1,5 +1,4 @@
 from action_sequence import ActionSequence
-from actions.click_action import ClickAction
 from actions.custom_action import CustomAction
 from actions.page_action import PageAction
 from actions.wait_action import WaitAction
@@ -15,10 +14,17 @@ class SubscribeUnsubscribeAction(ActionSequence):
 
         self.add_action(PageAction('discovered'))
         self.add_action(WaitAction(1000))
-        self.add_action(CustomAction("""if window.discovered_channels_list.count() == 0:
+        self.add_action(CustomAction("""if window.discovered_channels_list.model().rowCount() == 0:
     exit_script()
     """))
-        self.add_action(ClickAction('window.discovered_channels_list.itemWidget(window.discovered_channels_list.item(randint(0, window.discovered_channels_list.count() - 1))).subscribe_button'))
+
+        self.add_action(CustomAction("""table_view = window.discovered_channels_list
+random_row = randint(0, table_view.model().rowCount() - 1)
+x = table_view.columnViewportPosition(0)
+y = table_view.rowViewportPosition(random_row)
+index = table_view.indexAt(QPoint(x, y))
+table_view.on_subscribe_control_clicked(index)
+        """))
 
     def required_imports(self):
-        return ["from random import randint"]
+        return ["from PyQt5.QtCore import QPoint", "from random import randint"]
