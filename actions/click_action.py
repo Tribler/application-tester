963c5894fd58d230248c9513edd1be722478b320
Martijn de Vos <mdmartijndevos@gmail.com>
2019-01-29 16:00:22 +0100

    Made the application tester gigachannel compatible

--- actions/click_action.py
+++ actions/click_action.py
@@ -38,3 +38,51 @@ class ClickSequenceAction(Action):
 
     def required_imports(self):
         return ["from PyQt5.QtTest import QTest", "from PyQt5.QtCore import Qt"]
+
+
+class TableViewClickAction(Action):
+    """
+    This action clicks on a specific row in a table view.
+    """
+
+    def __init__(self, table_view_obj_name, row_index):
+        super(TableViewClickAction, self).__init__()
+        self.table_view_obj_name = table_view_obj_name
+        self.row_index = row_index
+
+    def action_code(self):
+        code = """table_view = %s
+x = table_view.columnViewportPosition(0)
+y = table_view.rowViewportPosition(%d)
+index = table_view.indexAt(QPoint(x, y))
+table_view.on_table_item_clicked(index)
+        """ % (self.table_view_obj_name, self.row_index)
+
+        return code
+
+    def required_imports(self):
+        return ["from PyQt5.QtCore import QPoint"]
+
+
+class RandomTableViewClickAction(Action):
+    """
+    This action clicks on a random row in a table view.
+    """
+
+    def __init__(self, table_view_obj_name):
+        super(RandomTableViewClickAction, self).__init__()
+        self.table_view_obj_name = table_view_obj_name
+
+    def action_code(self):
+        code = """table_view = %s
+random_row = randint(0, table_view.model().rowCount() - 1)
+x = table_view.columnViewportPosition(0)
+y = table_view.rowViewportPosition(random_row)
+index = table_view.indexAt(QPoint(x, y))
+table_view.on_table_item_clicked(index)
+        """ % self.table_view_obj_name
+
+        return code
+
+    def required_imports(self):
+        return ["from PyQt5.QtCore import QPoint", "from random import randint"]
