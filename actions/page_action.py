b4b2d5e9854674806c1e9caead2e641e391758a6
Martijn de Vos <mdmartijndevos@gmail.com>
2019-01-29 12:55:33 +0100

    Improved random page action

--- actions/page_action.py
+++ actions/page_action.py
@@ -1,26 +1,28 @@
 from random import choice
 
-from actions.click_action import ClickAction
+from actions.click_action import ClickSequenceAction
 
 
-class PageAction(ClickAction):
+class PageAction(ClickSequenceAction):
     """
     This action goes to a specific page in Tribler.
     """
     BUTTONS_TO_PAGES = {
-        'home': 'window.left_menu_button_home',
-        'discovered': 'window.left_menu_button_discovered',
-        'downloads': 'window.left_menu_button_downloads',
-        'my_channel': 'window.left_menu_button_my_channel',
-        'search': 'window.left_menu_button_search',
-        'subscriptions': 'window.left_menu_button_subscriptions',
-        'video_player': 'window.left_menu_button_video_player',
-        'token_balance': 'window.token_balance_widget',
-        'settings': 'window.settings_button'
+        'home': ['window.left_menu_button_home'],
+        'discovered': ['window.left_menu_button_discovered'],
+        'downloads': ['window.left_menu_button_downloads'],
+        'my_channel': ['window.left_menu_button_my_channel'],
+        'search': ['window.left_menu_button_search'],
+        'subscriptions': ['window.left_menu_button_subscriptions'],
+        'video_player': ['window.left_menu_button_video_player'],
+        'token_balance': ['window.token_balance_widget'],
+        'settings': ['window.settings_button'],
+        'market': ['window.token_balance_widget', 'window.trade_button'],
+        'token_mining': ['window.token_balance_widget', 'window.mine_button'],
     }
 
     def __init__(self, page_name):
-        super(PageAction, self).__init__(self.BUTTONS_TO_PAGES[page_name], left_button=True)
+        super(PageAction, self).__init__(self.BUTTONS_TO_PAGES[page_name])
 
 
 class RandomPageAction(PageAction):
