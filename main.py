d20b7d38eda6bd8454dd73c8025eb2c734595e5a
Martijn de Vos <mdmartijndevos@gmail.com>
2019-01-29 12:13:08 +0100

    Logging IPv8 peer count

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--silent', action='store_true', help='do not execute random actions')
     parser.add_argument('--monitordownloads', default=None, type=int, help='monitor the downloads with a specified interval in seconds')
     parser.add_argument('--monitorresources', default=None, type=int, help='monitor the resources with a specified interval in seconds')
+    parser.add_argument('--monitoripv8', default=None, type=int, help='monitor IPv8 overlays with a specified interval in seconds')
     parser.add_argument('--magnetsfile', default=os.path.join('data', 'torrent_links.txt'), type=str, help='specify the location of the file with magnet links')
 
     args = parser.parse_args()
