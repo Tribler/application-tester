b322f584fee39b0dbcaa21fe1b20bb96b1edc437
Martijn de Vos <mdmartijndevos@gmail.com>
2020-01-19 16:18:22 +0100

    Made code path an optional argument
    It now defaults to 5000.

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from tribler_apptester.executor import Executor
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a Tribler application test.')
     parser.add_argument('tribler_executable', metavar='path', type=str, help='the full path to the Tribler executable')
-    parser.add_argument('code_port', metavar='port', type=int, help='the port used to execute code')
     parser.add_argument('-p', '--plain', action='store_true', help='allow plain downloads')
     parser.add_argument('-d', '--duration', default=None, type=int, help='run the Tribler application tester for a specific period of time')
     parser.add_argument('-s', '--silent', action='store_true', help='do not execute random actions')
+    parser.add_argument('--codeport', default=5000, type=int, help='the port used to execute code')
     parser.add_argument('--monitordownloads', default=None, type=int, help='monitor the downloads with a specified interval in seconds')
     parser.add_argument('--monitorresources', default=None, type=int, help='monitor the resources with a specified interval in seconds')
     parser.add_argument('--monitoripv8', default=None, type=int, help='monitor IPv8 overlays with a specified interval in seconds')
