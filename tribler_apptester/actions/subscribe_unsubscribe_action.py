bbe7c8a20d9cb24151ed460d5c0e5dcda76a820a
Martijn de Vos <mdmartijndevos@gmail.com>
2020-04-02 14:13:29 +0200

    Fixed subscribe/unsubscribe action

--- tribler_apptester/actions/subscribe_unsubscribe_action.py
+++ tribler_apptester/actions/subscribe_unsubscribe_action.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from tribler_apptester.action_sequence import ActionSequence
+from tribler_apptester.actions.click_action import ClickAction, RandomTableViewClickAction
 from tribler_apptester.actions.custom_action import CustomAction
 from tribler_apptester.actions.page_action import PageAction
 from tribler_apptester.actions.wait_action import WaitAction
@@ -15,18 +16,16 @@ class SubscribeUnsubscribeAction(ActionSequence):
         super(SubscribeUnsubscribeAction, self).__init__()
 
         self.add_action(PageAction('discovered'))
-        self.add_action(WaitAction(1000))
-        self.add_action(CustomAction("""if window.discovered_channels_list.model().rowCount() == 0:
+        self.add_action(WaitAction(2000))
+        self.add_action(CustomAction("""if window.discovered_page.content_table.model().rowCount() == 0:
     exit_script()
     """))
+        self.add_action(RandomTableViewClickAction('window.discovered_page.content_table'))
+        self.add_action(WaitAction(2000))
 
-        self.add_action(CustomAction("""table_view = window.discovered_channels_list
-random_row = randint(0, table_view.model().rowCount() - 1)
-x = table_view.columnViewportPosition(0)
-y = table_view.rowViewportPosition(random_row)
-index = table_view.indexAt(QPoint(x, y))
-table_view.on_subscribe_control_clicked(index)
-        """))
+        self.add_action(ClickAction('window.discovered_page.subscribe_button'))
+        self.add_action(WaitAction(2000))
+        self.add_action(ClickAction('window.discovered_page.channel_back_button'))
 
     def required_imports(self):
-        return ["from PyQt5.QtCore import QPoint", "from random import randint"]
+        return ["from random import randint"]
