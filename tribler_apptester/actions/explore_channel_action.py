d786b08b9a88f3b3aee91cd04f5030f28cf63765
Martijn de Vos <mdmartijndevos@gmail.com>
2020-04-02 13:57:07 +0200

    Fixed explore channel action

--- tribler_apptester/actions/explore_channel_action.py
+++ tribler_apptester/actions/explore_channel_action.py
@@ -4,7 +4,6 @@ from tribler_apptester.action_sequence import ActionSequence
 from tribler_apptester.actions.click_action import ClickAction, RandomTableViewClickAction
 from tribler_apptester.actions.custom_action import CustomAction
 from tribler_apptester.actions.page_action import PageAction
-from tribler_apptester.actions.select_action import TableViewRandomSelectAction
 from tribler_apptester.actions.wait_action import WaitAction
 
 
@@ -18,20 +17,16 @@ class ExploreChannelAction(ActionSequence):
 
         self.add_action(PageAction('discovered'))
         self.add_action(WaitAction(1000))
-        self.add_action(CustomAction("""if window.discovered_channels_list.model().rowCount() == 0:
+        self.add_action(CustomAction("""if window.discovered_page.content_table.model().rowCount() == 0:
     exit_script()
         """))
-        self.add_action(RandomTableViewClickAction('window.discovered_channels_list'))
-        self.add_action(WaitAction(2500))
-
-        # We now click a random torrent to initiate the torrent checker
-        self.add_action(CustomAction("""if window.channel_page_container.content_table.model().rowCount() == 0:
-    exit_script()
-        """))
-
-        self.add_action(TableViewRandomSelectAction('window.channel_page_container.content_table'))
+        self.add_action(RandomTableViewClickAction('window.discovered_page.content_table'))
         self.add_action(WaitAction(2000))
-        self.add_action(ClickAction('window.channel_back_button'))
+
+        # We now click the preview button and go back to the previous page
+        self.add_action(ClickAction('window.discovered_page.channel_preview_button'))
+        self.add_action(WaitAction(2000))
+        self.add_action(ClickAction('window.discovered_page.channel_back_button'))
 
     def required_imports(self):
         return ["from random import randint"]
