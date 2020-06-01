99f60e62111993aef3a05fb0f4c7b42715697ced
Martijn de Vos <mdmartijndevos@gmail.com>
2020-06-01 16:10:53 +0200

    Fixed Tribler state path on Windows

--- tribler_apptester/utils/osutils.py
+++ tribler_apptester/utils/osutils.py
@@ -1,32 +1,21 @@
 import sys
 from pathlib import Path
 
+
+def get_home_dir():
+    return Path().home()
+
+
 if sys.platform == "win32":
-    try:
-        from win32com.shell import shell, shellcon
-
-        def get_home_dir():
-            return Path(shell.SHGetSpecialFolderPath(0, shellcon.CSIDL_PROFILE))
-
-        def get_appstate_dir():
-            return Path(shell.SHGetSpecialFolderPath(0, shellcon.CSIDL_APPDATA))
-
-    except ImportError:
-        def get_home_dir():
-            return Path().home()
-
-        def get_appstate_dir():
-            homedir = get_home_dir()
-            winversion = sys.getwindowsversion()
-            if winversion[0] == 6:
-                appdir = homedir / "AppData" / "Roaming"
-            else:
-                appdir = homedir / "Application Data"
-            return appdir
+    def get_appstate_dir():
+        homedir = get_home_dir()
+        winversion = sys.getwindowsversion()
+        if winversion[0] == 6:
+            appdir = homedir / "AppData" / "Roaming" / ".Tribler"
+        else:
+            appdir = homedir / "Application Data" / ".Tribler"
+        return appdir
 else:
     # linux or darwin (mac)
-    def get_home_dir():
-        return Path().home()
-
     def get_appstate_dir():
         return get_home_dir() / ".Tribler"
