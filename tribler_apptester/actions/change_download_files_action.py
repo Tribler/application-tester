165345c0d59a6a70962323b9a162a5efab222e8b
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2021-11-11 11:41:25 +0100

    Fix ChangeDownloadFilesAction

--- tribler_apptester/actions/change_download_files_action.py
+++ tribler_apptester/actions/change_download_files_action.py
@@ -22,15 +22,20 @@ class ChangeDownloadFilesAction(ActionSequence):
         self.add_action(WaitAction(2000))
         self.add_action(CustomAction('window.download_details_widget.setCurrentIndex(1)'))
         self.add_action(WaitAction(2000))
-        self.add_action(CustomAction("""if window.download_files_list.topLevelItemCount() == 0:
+        self.add_action(CustomAction("""
+tree_view = window.download_files_list
+if tree_view.topLevelItemCount() == 0:
     exit_script()
-        """))
-        self.add_action(CustomAction("item = window.download_files_list.topLevelItem(randint(0, window.download_files_list.topLevelItemCount() - 1))"))
-        self.add_action(CustomAction("""if item.file_info['included']:
-    window.download_details_widget.on_files_excluded([item.file_info])
-else:
-    window.download_details_widget.on_files_included([item.file_info])
+item = tree_view.topLevelItem(randint(0, tree_view.topLevelItemCount() - 1))
+check_state = Qt.Checked if item.checkState(CHECKBOX_COL) == Qt.Unchecked else Qt.Unchecked
+item.setCheckState(CHECKBOX_COL, check_state)
+QMetaObject.invokeMethod(tree_view, "itemClicked", Q_ARG(QTreeWidgetItem, item), Q_ARG(int, CHECKBOX_COL))
         """))
 
     def required_imports(self):
-        return ["from random import randint"]
+        return [
+            "from random import randint",
+            "from PyQt5.QtCore import QMetaObject, Q_ARG, Qt",
+            "from PyQt5.QtWidgets import QTreeWidgetItem",
+            "from tribler_gui.widgets.torrentfiletreewidget import CHECKBOX_COL"
+        ]
