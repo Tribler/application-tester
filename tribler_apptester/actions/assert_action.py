c6bb92d905f06c4341cadfef201541a36cc73789
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2021-12-17 00:25:43 +0100

    Take screenshot on failed assertion

--- tribler_apptester/actions/assert_action.py
+++ tribler_apptester/actions/assert_action.py
@@ -1,6 +1,7 @@
 from tribler_apptester.action_sequence import ActionSequence
 from tribler_apptester.actions.conditional_action import ConditionalAction
 from tribler_apptester.actions.custom_action import CustomAction
+from tribler_apptester.actions.screenshot_action import ScreenshotAction
 
 
 class AssertAction(ActionSequence):
@@ -9,5 +10,8 @@ class AssertAction(ActionSequence):
         super().__init__()
         self.add_action(ConditionalAction(
             condition=test_condition,
-            else_action=CustomAction(f"raise AssertionError('Assertion fails: %s' % {test_condition!r})"))
-        )
+            else_action=ActionSequence(actions=[
+                ScreenshotAction(),
+                CustomAction(f"raise AssertionError('Assertion fails: %s' % {test_condition!r})")
+            ])
+        ))
